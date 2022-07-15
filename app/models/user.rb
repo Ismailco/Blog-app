af4cbb8dcb92df99df3151f2b111e8aa66b603c7
Ismail Courr <contact@ismailcourr.ml>
2022-07-15 10:46:01 +0100

    Add only_integer to validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: :author_id
 
   validates :name, presence: true
-  validates :posts_counter, numericality: { greater_then_or_equal_to: 0 }
+  validates :posts_counter, numericality: { only_integer: true, greater_then_or_equal_to: 0 }
 
   def last_three_posts
     posts.includes(:author).order(created_at: :DESC).limit(3)
