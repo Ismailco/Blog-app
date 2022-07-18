67260b092f505cb7d595b9cf4e61b2dcd9b26156
Ismail Courr <contact@ismailcourr.ml>
2022-07-18 13:11:35 +0100

    generate controllers for pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def users
+  end
+
+  def posts
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/users'
+  get 'pages/posts'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
