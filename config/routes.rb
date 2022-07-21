7e7645e469b4f569ad3ee9fe7346b6a54faa26ee
Ismail Courr <contact@ismailcourr.ml>
2022-07-21 10:32:50 +0100

    Add testes for the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'users#index'
+  get '/users', to: 'users#index'
   get '/users/:user_id', to: 'users#show'
   get '/users/:user_id/posts', to: 'posts#index'
   get '/users/:user_id/posts/:post_id', to: 'posts#show'
