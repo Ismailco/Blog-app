bf0ea7b8b6c8ba092eb4a806d499dae7505a1b81
Ismail Courr <contact@ismailcourr.ml>
2022-07-22 21:03:44 +0100

    Finish project

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,38 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[show edit update destroy]
+
   def index
     @user = User.find(params[:user_id])
-    @post = @user.posts
+    @posts = @user.posts
   end
 
-  def show
-    @user = User.find(params[:user_id])
-    @post = @user.posts.find(params[:post_id])
+  def show; end
+
+  def new
+    @post = Post.new(author_id: params[:user_id], comments_counter: 0, likes_counter: 0)
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to user_posts_path, notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:author_id, :title, :text, :comments_counter, :likes_counter)
   end
 end
