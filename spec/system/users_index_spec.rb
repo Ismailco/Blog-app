0a7ac31a9e85d3708a96bd5bbe1a6c2907f3bda0
Ismail Courr <contact@ismailcourr.ml>
2022-07-26 14:06:20 +0100

    Finish the user index tests

--- spec/system/users_index_spec.rb
+++ spec/system/users_index_spec.rb
@@ -9,12 +9,28 @@ RSpec.describe 'Users Page' do
                          photo: 'https://eitrawmaterials.eu/wp-content/uploads/2016/09/person-icon.png')
   end
 
-  describe 'Test user index' do
+  describe 'Test user index and expect to: ' do
+    def create_user
+      visit '/users'
+      click_link 'New user'
+      fill_in 'Name', with: 'Ismail'
+      fill_in 'Bio', with: 'This is my bio'
+      fill_in 'Photo', with: 'icon.png'
+      fill_in 'Posts counter', with: '0'
+      click_button 'Create User'
+    end
+
+    it 'create a new user' do
+      create_user
+      expect(page).to have_content('Ismail')
+      expect(page).to have_content('Number of posts: 0')
+    end
+
     it 'Shows the username' do
       expect(@user1.name).to have_content('John')
     end
 
-    it "Shows the user's photo" do
+    it 'Shows the user picture' do
       all('img').each do |i|
         expect(i[:src]).to eq('https://eitrawmaterials.eu/wp-content/uploads/2016/09/person-icon.png')
       end
@@ -24,6 +40,16 @@ RSpec.describe 'Users Page' do
       all(:css, '.num_post').each do |post|
         expect(post).to have_content('Number of posts: 0')
       end
+    end
+
+    it 'Redirect to the user show page' do
+      create_user
+      click_link 'Ismail'
+      expect(page).to have_content('Ismail')
+      expect(page).to have_content('Number of posts: 0')
+      expect(page).to have_content('This is my bio')
+      expect(page).to have_content('See all posts')
+      expect(page).to have_content('new')
     end
   end
 end
