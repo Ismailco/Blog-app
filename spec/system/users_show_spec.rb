d66ba215ee32347fb30971fce89ddf191b267308
Ismail Courr <contact@ismailcourr.ml>
2022-07-26 13:08:07 +0100

    Fix testing the user show page

--- spec/system/users_show_spec.rb
+++ spec/system/users_show_spec.rb
@@ -5,50 +5,35 @@ RSpec.describe 'Users Page' do
     driven_by(:rack_test)
   end
   before(:each) do
-    @user = User.create(id: 1, name: 'Ismail', bio: 'This is my bio',
-                        photo: 'icon.png')
-    3.times do
-      Post.create(user_id: 1, text: 'This is my post')
-    end
+    visit '/users'
+    click_link 'New user'
+    fill_in 'Name', with: 'Ismail'
+    fill_in 'Bio', with: 'This is my bio'
+    fill_in 'Photo', with: 'icon.png'
+    fill_in 'Posts counter', with: '0'
+    click_button 'Create User'
   end
 
-  describe 'Test user show' do
-    it 'Shows the username' do
-      expect(@user.name).to have_content('Ismail')
-    end
-
-    it "Shows the user's photo" do
+  describe 'Test user and expect to: ' do
+    it 'See the user profile picture' do
       all('img').each do |i|
         expect(i[:src]).to eq('icon.png')
       end
     end
 
-    it 'Shows the number of posts' do
+    it 'See the user username' do
+      expect(page).to have_content('Ismail')
+    end
+
+    it 'See the number of posts the user created' do
       all(:css, '.num_post').each do |post|
         expect(post).to have_content('Number of posts: 0')
       end
     end
 
-    it 'Shows the bio' do
-      expect(@user.bio).to have_content('This is my bio')
-    end
-
-    it 'shows the button to all posts' do
-      @user.posts.each do |post|
-        expect(post).to have_content('All posts')
-      end
-    end
-
-    it 'shows the button to all comments' do
-      @user.comments.each do |comment|
-        expect(comment).to have_content('All comments')
-      end
-    end
-
-    it 'shows the button to all likes' do
-      @user.likes.each do |like|
-        expect(like).to have_content('All likes')
-      end
+    it 'See the user bio' do
+      click_link 'Ismail'
+      expect(page).to have_content('This is my bio')
     end
   end
 end
