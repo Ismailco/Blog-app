c17d818d4771599c8f42bad7db4121bce7d071fd
Ismail Courr <contact@ismailcourr.ml>
2022-07-26 09:00:52 +0100

    fix linters

--- spec/system/users_show_spec.rb
+++ spec/system/users_show_spec.rb
@@ -1,51 +1,55 @@
 require 'rails_helper'
 
-RSpec.feature 'Users Page' do
-  feature 'shows users' do
-    background do
-      @user = User.create(id: 1, name: 'John', bio: 'This is my bio',
-                          photo: 'icon.png')
-      3.times do
-        Post.create(user_id: 1, text: 'This is my post')
-      end
+RSpec.describe 'Users Page' do
+  before do
+    driven_by(:rack_test)
+  end
+  before(:each) do
+    @user = User.create(id: 1, name: 'Ismail', bio: 'This is my bio',
+                        photo: 'icon.png')
+    3.times do
+      Post.create(user_id: 1, text: 'This is my post')
+    end
+  end
+
+  describe 'Test user show' do
+    it 'Shows the username' do
+      expect(@user.name).to have_content('Ismail')
     end
 
-    scenario 'Shows the username' do
-      expect(@user.name).to have_content('John')
-    end
-
-    scenario "Shows the user's photo" do
+    it "Shows the user's photo" do
       all('img').each do |i|
         expect(i[:src]).to eq('icon.png')
       end
     end
 
-    scenario 'Shows the number of posts' do
+    it 'Shows the number of posts' do
       all(:css, '.num_post').each do |post|
         expect(post).to have_content('Number of posts: 0')
       end
     end
 
-    scenario 'Shows the bio' do
+    it 'Shows the bio' do
       expect(@user.bio).to have_content('This is my bio')
     end
 
-    scenario 'shows the button to all posts' do
+    it 'shows the button to all posts' do
       @user.posts.each do |post|
         expect(post).to have_content('All posts')
       end
     end
 
-    scenario 'shows the button to all comments' do
+    it 'shows the button to all comments' do
       @user.comments.each do |comment|
         expect(comment).to have_content('All comments')
       end
     end
 
-    scenario 'shows the button to all likes' do
+    it 'shows the button to all likes' do
       @user.likes.each do |like|
         expect(like).to have_content('All likes')
       end
     end
+
   end
 end
