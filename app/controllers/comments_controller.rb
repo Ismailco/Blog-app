537d2e7f6992cfcec4a26bbb8fe5b2dc5d4fa9b4
Ismail Courr <contact@ismailcourr.ml>
2022-07-26 17:11:29 +0100

    File structure

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -23,16 +23,6 @@ class CommentsController < ApplicationController
     end
   end
 
-  # def create
-  #   @comment = current_user.comments.new(comment_params)
-  #   @comment.post_id = params[:post_id]
-  #   if @comment.save
-  #     redirect_to user_post_path(current_user.id, params[:post_id])
-  #   else
-  #     render :create
-  #   end
-  # end
-
   private
 
   def comment_params
