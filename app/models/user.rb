075592968aa0534c1dbc965e5ca7b6cb99d5a3c1
Ismail Courr <contact@ismailcourr.ml>
2022-07-27 21:15:33 +0100

    Fix sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
 
-  validates :name, presence: true
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :name, presence: true, allow_blank: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
 
   def last_three_posts
     posts.includes(:author).order(created_at: :DESC).limit(3)
